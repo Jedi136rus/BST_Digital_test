87c19eecfcf6018601e790a75ff4b37545aac1bc
Jedi136rus <chay-ka2008@yandex.ru>
2023-09-29 22:31:47 +0300

    [fix] dubles don't need to check

--- robots/views.py
+++ robots/views.py
@@ -22,22 +22,16 @@ class RobotsApi(View):
         if new_obj.is_valid():
             # если поле serial не передалось составим его из model и version
             if not data.get("serial", None):
-                # проверим создание экземпляра на наличие дублей
-                if Robot.objects.filter(model=data['model'], version=data['version']).exists():
-                    return JsonResponse({'response': f"{data['model']}-{data['version']} is already exist",
-                                         'result': 'error'}, status=404)
-                else:
-                    data['serial'] = f"{data['model']}-{data['version']}"
-                    new_robot = Robot.objects.create(**data)
-                    new_robot.save()
-                    return JsonResponse({'response': data,
-                                         'result': 'ok'}, status=200)
+                data['serial'] = f"{data['model']}-{data['version']}"
+            new_robot = Robot.objects.create(**data)
+            new_robot.save()
+            return JsonResponse({'response': data,
+                                 'result': 'ok'}, status=200)
         else:
             # соберем ошибки в переданных полях
             response = {}
             for k in new_obj.errors:
                 response[k] = new_obj.errors[k][0]
             return JsonResponse({'response': response, 'result': 'error'}, status=404)
-        return JsonResponse({'msg': 'ok'}, status=200)
 
 
