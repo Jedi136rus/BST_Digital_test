acaefc41632a6f2d31f6a30b88b3b64429a8c163
Jedi136rus <chay-ka2008@yandex.ru>
2023-09-30 01:03:31 +0300

    second task

--- robots/urls.py
+++ robots/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 # from .views import api_posts
-from .views import RobotsApi
+from .views import RobotsApi, excel_report
 
 urlpatterns = [
     # path('add/', api_posts),
     path('add/', RobotsApi.as_view()),
+    path('excel_report/', excel_report)
 ]
